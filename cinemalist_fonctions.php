647f315ce68856b480aac64f2917c5cf34a56bac
abelass <websolutions@mychacra.net>
2012-12-17 20:15:24 +0100

    traduire des chacractère no migrés

--- cinemalist_fonctions.php
+++ cinemalist_fonctions.php
@@ -21,4 +21,90 @@ function transformer_prenom($prenom='',$nom,$id,$objet){
 		return $prenom;
 	}
 }
+
+function unhtmlentities($string)
+    {
+   // replace numeric entities
+   $string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
+   $string = preg_replace('~&#([0-9]+);~e', 'chr(\\1)', $string);
+   // replace literal entities
+   $trans_tbl = get_html_translation_table(HTML_ENTITIES);
+   $trans_tbl = array_flip($trans_tbl);
+   return strtr($string, $trans_tbl);
+    }
+/*Ã© ---> é
+Ã´ ---> ô
+Ã« ---> ë
+Ã¨ ---> è
+Ã ----> à
+Ãª ----> ê
+â€™ ---> '*/
+function filtrer_iso($string){
+   // replace numeric entities
+   $patterns = array(
+        '/Ã©/', //remplace le tag
+        '/Ã´/',
+        '/Ã«/',
+        '/Ã¨/',
+        '/Ã/',
+        '/Ãª/',
+        '/â€™/',
+        );
+   $replace = array(
+        'é', //remplace le tag
+        'ô',
+        'ë',
+        'è',
+        'à',
+        'ê',
+        "'",
+        );        
+   $return = preg_replace($patterns,$replace, $string);
+
+   return $return;
+    } 
+function JsToDb( $string)
+{
+    
+       $patterns = array(
+        '/&Atilde;&copy;/', //remplace le tag
+        '/&Atilde;&uml;/',
+        '/&Atilde;&ordf;/',
+        '/&Atilde;&laquo/',
+        '/&Atilde;&nbsp;/',
+        '/&Atilde;&curren;/',
+        '/&Atilde;&cent;/',
+        '/&Atilde;&sup1;/',   
+        '/&Atilde;&raquo;/', 
+        '/&Atilde;&frac14;/',
+        '/&Atilde;&acute;/',  
+        '/&Atilde;&para;/', 
+        '/&Atilde;&reg;/', 
+        '/&Atilde;&macr;/', 
+        '/&Atilde;&sect;/',                                                                         
+        );
+        
+    $replace = array(
+        '&eacute;', //remplace le tag
+        'è',
+        '&ecirc;',
+        '&euml;',
+        '&agrave;',
+        '&auml;',
+        '&acirc;',
+        '&ugrave;', 
+        '&ucirc;',  
+        '&uuml;',
+        '&ocirc;',  
+        '&ouml;',  
+        '&icirc;', 
+        '&iuml;', 
+        '&ccedil;',                                                               
+        );         
+
+
+
+      $string= preg_replace($patterns,$replace, $string);
+    return  $string;
+}   
 ?>
