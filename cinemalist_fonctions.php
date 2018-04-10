0e2677a3deed8cf78a00bb5263145023c60e668a
abelass <rainer@websimple.be>
2018-04-10 16:10:26 +0200

    nettoyage

--- cinemalist_fonctions.php
+++ cinemalist_fonctions.php
@@ -3,14 +3,14 @@
 // Filtre qui permet de récouper et redimensionner une imag/log
 
 function image_reduire_recadre($img, $largeur, $hauteur, $position='center') {
-       include_spip('inc/filtres_images');
-       if ($img!='IMG/'){
-            list ($ret["hauteur"],$ret["largeur"]) = taille_image($img);
-            $ratio_x = $ret["largeur"]/$largeur;
-            $ratio_y = $ret["hauteur"]/$hauteur;
-            $ratio   = ($ratio_x <= $ratio_y) ? $ratio_x : $ratio_y;
-            return image_recadre(image_reduire_par($img, $ratio), $largeur, $hauteur, $position);
-            }
+			 include_spip('inc/filtres_images');
+			 if ($img!='IMG/'){
+						list ($ret["hauteur"],$ret["largeur"]) = taille_image($img);
+						$ratio_x = $ret["largeur"]/$largeur;
+						$ratio_y = $ret["hauteur"]/$hauteur;
+						$ratio   = ($ratio_x <= $ratio_y) ? $ratio_x : $ratio_y;
+						return image_recadre(image_reduire_par($img, $ratio), $largeur, $hauteur, $position);
+						}
 }
 
 // filtre transitoire pour intégrer les prenoms dans le nom
@@ -23,15 +23,15 @@ function transformer_prenom($prenom='',$nom,$id,$objet){
 }
 
 function unhtmlentities($string)
-    {
-   // replace numeric entities
-   $string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
-   $string = preg_replace('~&#([0-9]+);~e', 'chr(\\1)', $string);
-   // replace literal entities
-   $trans_tbl = get_html_translation_table(HTML_ENTITIES);
-   $trans_tbl = array_flip($trans_tbl);
-   return strtr($string, $trans_tbl);
-    }
+		{
+	 // replace numeric entities
+	 $string = preg_replace('~&#x([0-9a-f]+);~ei', 'chr(hexdec("\\1"))', $string);
+	 $string = preg_replace('~&#([0-9]+);~e', 'chr(\\1)', $string);
+	 // replace literal entities
+	 $trans_tbl = get_html_translation_table(HTML_ENTITIES);
+	 $trans_tbl = array_flip($trans_tbl);
+	 return strtr($string, $trans_tbl);
+		}
 /*Ã© ---> é
 Ã´ ---> ô
 Ã« ---> ë
@@ -40,88 +40,88 @@ function unhtmlentities($string)
 Ãª ----> ê
 â€™ ---> '*/
 function filtrer_iso($string){
-   // replace numeric entities
-   $patterns = array(
-        '/Ã©/', //remplace le tag
-        '/Ã´/',
-        '/Ã«/',
-        '/Ã¨/',
-        '/Ã/',
-        '/Ãª/',
-        '/â€™/',
-        );
-   $replace = array(
-        'é', //remplace le tag
-        'ô',
-        'ë',
-        'è',
-        'à',
-        'ê',
-        "'",
-        );        
-   $return = preg_replace($patterns,$replace, $string);
+	 // replace numeric entities
+	 $patterns = array(
+				'/Ã©/', //remplace le tag
+				'/Ã´/',
+				'/Ã«/',
+				'/Ã¨/',
+				'/Ã/',
+				'/Ãª/',
+				'/â€™/',
+				);
+	 $replace = array(
+				'é', //remplace le tag
+				'ô',
+				'ë',
+				'è',
+				'à',
+				'ê',
+				"'",
+				);
+	 $return = preg_replace($patterns,$replace, $string);
 
-   return $return;
-    } 
+	 return $return;
+		}
 function JsToDb( $string)
 {
-    
-       $patterns = array(
-        '/&Atilde;&copy;/', //remplace le tag
-        '/&Atilde;&uml;/',
-        '/&Atilde;&ordf;/',
-        '/&Atilde;&laquo/',
-        '/&Atilde;&nbsp;/',
-        '/&Atilde;&curren;/',
-        '/&Atilde;&cent;/',
-        '/&Atilde;&sup1;/',   
-        '/&Atilde;&raquo;/', 
-        '/&Atilde;&frac14;/',
-        '/&Atilde;&acute;/',  
-        '/&Atilde;&para;/', 
-        '/&Atilde;&reg;/', 
-        '/&Atilde;&macr;/', 
-        '/&Atilde;&sect;/',                                                                         
-        );
-        
-    $replace = array(
-        '&eacute;', //remplace le tag
-        'è',
-        '&ecirc;',
-        '&euml;',
-        '&agrave;',
-        '&auml;',
-        '&acirc;',
-        '&ugrave;', 
-        '&ucirc;',  
-        '&uuml;',
-        '&ocirc;',  
-        '&ouml;',  
-        '&icirc;', 
-        '&iuml;', 
-        '&ccedil;',                                                               
-        );         
+
+			 $patterns = array(
+				'/&Atilde;&copy;/', //remplace le tag
+				'/&Atilde;&uml;/',
+				'/&Atilde;&ordf;/',
+				'/&Atilde;&laquo/',
+				'/&Atilde;&nbsp;/',
+				'/&Atilde;&curren;/',
+				'/&Atilde;&cent;/',
+				'/&Atilde;&sup1;/',
+				'/&Atilde;&raquo;/',
+				'/&Atilde;&frac14;/',
+				'/&Atilde;&acute;/',
+				'/&Atilde;&para;/',
+				'/&Atilde;&reg;/',
+				'/&Atilde;&macr;/',
+				'/&Atilde;&sect;/',
+				);
+
+		$replace = array(
+				'&eacute;', //remplace le tag
+				'è',
+				'&ecirc;',
+				'&euml;',
+				'&agrave;',
+				'&auml;',
+				'&acirc;',
+				'&ugrave;',
+				'&ucirc;',
+				'&uuml;',
+				'&ocirc;',
+				'&ouml;',
+				'&icirc;',
+				'&iuml;',
+				'&ccedil;',
+				);
 
 
 
-      $string= preg_replace($patterns,$replace, $string);
-    return  $string;
-}   
+			$string= preg_replace($patterns,$replace, $string);
+		return  $string;
+}
 
 function rediriger_ancien_url($url){
 
-    $explode=explode(',',$url);
-    $objets=array(realisateur,scenariste,acteur,film);
+		$explode=explode(',',$url);
+		$objets=array(realisateur,scenariste,acteur,film);
 
-    foreach($objets AS $objet){
-        if(preg_match('*'.$objet.'*',$explode[0])){
-            $id_objet=str_replace($objet,'',$explode[0]);
-            $url_new=str_replace('.html','', $url);
-            sql_insertq('spip_urls',array('type'=>$objet,'id_objet'=>$id_objet,'url'=>$url_new,'date'=>'2007-11-24 16:02:47'));
-            $redirect=generer_url_entite($objet,$id_objet);
-            header("location:$url");
-        }
-        
-    }
+		foreach($objets AS $objet){
+				if(preg_match('*'.$objet.'*',$explode[0])){
+						$id_objet=str_replace($objet,'',$explode[0]);
+						$url_new=str_replace('.html','', $url);
+						sql_insertq('spip_urls',array('type'=>$objet,'id_objet'=>$id_objet,'url'=>$url_new,'date'=>'2007-11-24 16:02:47'));
+						$redirect=generer_url_entite($objet,$id_objet);
+						header("location:$url");
+				}
+
+		}
 }
-?>
+
--- cinemalist_pipelines.php
+++ cinemalist_pipelines.php
@@ -1,153 +1,62 @@
 <?php
 
-
-/*
-	function cinemalist_declarer_url_objets($array){
-   	 	$array[] = 'acteur|film|scenariste|realisateur';  
-  	 	   	 	 	 	
-   	 return $array;
-	}*/
-
-    if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 
 	function cinemalist_affiche_milieu($flux){
-	    
-        $texte = "";
-    $e = trouver_objet_exec($flux['args']['exec']);
+
+				$texte = "";
+		$e = trouver_objet_exec($flux['args']['exec']);
 
 
 
-    // films sur les acteurs, articles, realisateurs, scenaristes
-    if (!$e['edition'] AND in_array($e['type'], array('article'))) {
-        $texte .= recuperer_fond('prive/objets/editer/liens', array(
-            'table_source' => 'films',
-            'objet' => $e['type'],
-            'id_objet' => $flux['args'][$e['id_table_objet']]
-        ));
-    }
-    // acteurs sur les films
-    if (!$e['edition'] AND in_array($e['type'], array('film'))) {
-        $texte .= recuperer_fond('prive/objets/editer/liens', array(
-            'table_source' => 'acteurs',
-            'objet' => $e['type'],
-            'id_objet' => $flux['args'][$e['id_table_objet']]
-        ));
-    }
-    // realisateurs sur les films
-    if (!$e['edition'] AND in_array($e['type'], array('film'))) {
-        $texte .= recuperer_fond('prive/objets/editer/liens', array(
-            'table_source' => 'realisateurs',
-            'objet' => $e['type'],
-            'id_objet' => $flux['args'][$e['id_table_objet']]
-        ));
-    }
-    // scenaristes sur les films
-    if (!$e['edition'] AND in_array($e['type'], array('film'))) {
-        $texte .= recuperer_fond('prive/objets/editer/liens', array(
-            'table_source' => 'scenaristes',
-            'objet' => $e['type'],
-            'id_objet' => $flux['args'][$e['id_table_objet']]
-        ));
-    }
+		// films sur les acteurs, articles, realisateurs, scenaristes
+		if (!$e['edition'] AND in_array($e['type'], array('article'))) {
+				$texte .= recuperer_fond('prive/objets/editer/liens', array(
+						'table_source' => 'films',
+						'objet' => $e['type'],
+						'id_objet' => $flux['args'][$e['id_table_objet']]
+				));
+		}
+		// acteurs sur les films
+		if (!$e['edition'] AND in_array($e['type'], array('film'))) {
+				$texte .= recuperer_fond('prive/objets/editer/liens', array(
+						'table_source' => 'acteurs',
+						'objet' => $e['type'],
+						'id_objet' => $flux['args'][$e['id_table_objet']]
+				));
+		}
+		// realisateurs sur les films
+		if (!$e['edition'] AND in_array($e['type'], array('film'))) {
+				$texte .= recuperer_fond('prive/objets/editer/liens', array(
+						'table_source' => 'realisateurs',
+						'objet' => $e['type'],
+						'id_objet' => $flux['args'][$e['id_table_objet']]
+				));
+		}
+		// scenaristes sur les films
+		if (!$e['edition'] AND in_array($e['type'], array('film'))) {
+				$texte .= recuperer_fond('prive/objets/editer/liens', array(
+						'table_source' => 'scenaristes',
+						'objet' => $e['type'],
+						'id_objet' => $flux['args'][$e['id_table_objet']]
+				));
+		}
 
-    /*// article sur les films
-    if (!$e['edition'] AND in_array($e['type'], array('film'))) {
-        $texte .= recuperer_fond('prive/objets/editer/liens', array(
-            'table_source' => 'articles',
-            'objet' => $e['type'],
-            'id_objet' => $flux['args'][$e['id_table_objet']]
-        ));
-    }*/
 
-    if ($texte) {
-        if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
-            $flux['data'] = substr_replace($flux['data'],$texte,$p,0);
-        else
-            $flux['data'] .= $texte;
-    }
-        
-		/*if ($flux['args']['exec'] == "article"){
-			$flux['data'] = 
-			$liaison_ok = false;
-			$id_film = _request('id_film');
-			$todo = _request('cinetic_action');
-			if (isset($todo)){
-				switch ($todo) {
-					case 'lier':
-						$query_test = "SELECT id FROM spip_lien_film_article WHERE id_article = '".$flux['args']['id_article']."'";
-						$result_test = mysql_query($query_test);
-						if (mysql_num_rows($result_test) >= 1) {
-							$flux['data'] .= "Le lien existe d&eacute;j&agrave; !";
-						}
-						else{
-							$query_insertion = "INSERT INTO spip_lien_film_article (id, id_article, id_film) VALUES ('', '".$flux['args']['id_article']."','".$id_film."')";
-							$result_insertion = mysql_query($query_insertion);
-							if (!($result_insertion)){
-								$flux['data'] .= "Liaison &eacute;chou&eacute;e..";
-							}
-						}
-						break;
-					
-					case 'delier':
-						$query_test = "DELETE FROM spip_lien_film_article WHERE id_article = '".$flux['args']['id_article']."'";
-						$result_test = mysql_query($query_test);
-						break;
-					
-					case 'autre':
-						$flux['data'] .= "Autre...";
-						break;
-				}
-			}
-			
-			$query_lien = "SELECT * FROM spip_lien_film_article WHERE id_article = '".$flux['args']['id_article']."'";
-			$result_lien = mysql_query($query_lien);
-			if (mysql_num_rows($result_lien) >= 1){
-				while ($row_lien = sql_fetch($result_lien)) {
-					$query_titre = "SELECT * FROM spip_films WHERE id_film = '".$row_lien['id_film']."' order by title";
-					$result_titre = mysql_query($query_titre);
-					while ($row_titre = sql_fetch($result_titre))
-					{
-						$titre_film = $row_lien['id_film']." - ".$row_titre['title'];
-					}
-				}
-				$liaison_ok = true;
-			}
-			
-			$flux['data'] .= debut_cadre_couleur(find_in_path('img_pack/mini_bobine.png'),true,'',_T('cinemalist:cinemalist'),'lien_film_'.$flux['args']['id_article'],'lien_film');
-			$flux['data'] .= "<br />";
-			if ($liaison_ok){	
-				$flux['data'] .= "<b>"._T('cinemalist:cette_critique_est_liee_au_film_suivant')."<br />";
-				
-				$flux['data'] .= "".$titre_film."</b>";
-				$flux['data'] .= '<form action="'.$_PHP_SELF.'" method="post">';
-				$flux['data'] .= "<input type='hidden' name='cinetic_action' value='delier' >";
-				$flux['data'] .= '<br /><input type="submit" value="Supprimer le lien." />';
-				$flux['data'] .= "</form>";
-			}
-			else{
-				$flux['data'] .= "<b>"._T('cinemalist:a_quel_film_voulez-vous_lier_cette_critique')."</b>";
-				$flux['data'] .= '<form action="'.$_PHP_SELF.'" method="post" >';
-				$flux['data'] .= '<select name="id_film">';
-				$sql = 'SELECT * FROM `spip_films` order by title';
-				$result_listearticle = mysql_query($sql);
-				while ($row = mysql_fetch_array($result_listearticle)) {
-					$flux['data'] .= '<option value="'.$row['id_film'].'" label="zorglub">'.$row['id_film'].'&nbsp;-&nbsp;'.$row['title'].'</option>';
-				}
-				$flux['data'] .= '</select>';
-				$flux['data'] .= "<input type='hidden' name='cinetic_action' value='lier' >";
-				$flux['data'] .= '<br /><input type="submit" value="Ok" size="10" />';
-				$flux['data'] .= '</form>';
-			}
-			
-			$flux['data'] .= fin_cadre_couleur(true);
-			$flux['data'] .= "<br />";
-		}*/
-				
+		if ($texte) {
+				if ($p=strpos($flux['data'],"<!--affiche_milieu-->"))
+						$flux['data'] = substr_replace($flux['data'],$texte,$p,0);
+				else
+						$flux['data'] .= $texte;
+		}
+
+
+
 		return $flux;
 	}
-	
+
 /**
  * Optimiser la base de donnees en supprimant les liens orphelins
  * de l'objet vers quelqu'un et de quelqu'un vers l'objet.
@@ -156,14 +65,13 @@
  * @return int
  */
 function cinemalist_optimiser_base_disparus($flux){
-    include_spip('action/editer_liens');
-    $flux['data'] += objet_optimiser_liens(array('film'=>'*', 'acteur'=>'*', 'realisateur'=>'*', 'scenariste'=>'*'),'*');
-    return $flux;
+		include_spip('action/editer_liens');
+		$flux['data'] += objet_optimiser_liens(array('film'=>'*', 'acteur'=>'*', 'realisateur'=>'*', 'scenariste'=>'*'),'*');
+		return $flux;
 }
 
 function cinemalist_jqueryui_plugins($scripts){
-   $scripts[] = 'jquery.ui.autocomplete';
-   return $scripts;
+	$scripts[] = 'jquery.ui.autocomplete';
+	return $scripts;
 }
 
-?>
